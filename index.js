4fd1d30045b768e2c9891ac6d0758f22c4caceb4
PhithLoveSandwich <664259020@webmail.npru.ac.th>
2024-07-30 16:01:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
+const port = 4000;
+
+//create server
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome, this is a webhook for Line Chatbot</h1>");
+});
+
+app.post("/webhook", (req, res) => {
+  //create webhook client
+  const agent = new WebhookClient({
+    request: req,
+    response: res,
+  });
+
+  console.log("Dialogflow Request headers: " + JSON.stringify(req.headers));
+  console.log("Dialogflow Request body: " + JSON.stringify(req.body));
+  function welcome(agent) {
+    agent.add();
+  }
+  function fallback(agent) {
+    agent.add();
+    agent.add();
+  }
+
+  function bodyMassIndex(agent) {
+    let weight = agent.parameters.weight;
+    let height = agent.parameters.height / 100;
+    let bmi = (weight / (height * height)).toFixed(2);
+    let result = "ขออภัย หนูไม่เข้าใจ";
+
+    if (bmi < 18.5) {
+      result = "คุณผอมไป กินข้าวบ้างนะ";
+    } else if (bmi >= 18.5 && bmi <= 22.9) {
+      result = "คุณหุ่นดีจุงเบย";
+    } else if (bmi >= 23 && bmi <= 24.9) {
+      result = "คุณเริ่มจะท้วมแล้วนะ";
+    } else if ((bmi >= 25.8) & (bmi <= 29.9)) {
+      result = "คุณอ้วนละ ออกกำลังกายหน่อยนะ";
+    } else if (bmi > 30) {
+      result = "คุณอ้วนเกินไปละ หาหมอเหอะ";
+    }
+    const flexMessage = {};
+
+    agent.add(result);
+  }
+
+  let intentMap = new Map();
+  intentMap.set("Default Welcome Intent", welcome);
+  intentMap.set("Default Fallback Intent", fallback);
+
+  intentMap.set("BMI - custom - yes", bodyMassIndex);
+
+  agent.handleRequest(intentMap);
+});
+app.listen(port, () => {
+  console.log("Server is running at http://localhost:" + port);
+});
